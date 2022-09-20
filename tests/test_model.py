30e056b43068347c81e26864d4d60aef4f045b02
Keelan O'Neill <keelan.oneill@uwa.edu.au>
2022-09-20 12:13:45 +0800

    update test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,13 @@
-from hydrogen_pfhx import model
+# simple test for verifying model
+import matplotlib.pyplot as plt    
+from hydrogen_pfhx import model, outputs
 
-model()
+# run the model with alt config
+results = model.model('src/configs/alternate_configuration.yaml')
+
+# print results
+print(results)
+
+# plot & display results!
+outputs.plot_results(results)
+plt.show()
